8e938000c724a94005d8702e8c3c636034edac7c
queenyaa <ysgyan@gmail.com>
2024-04-09 22:37:12 +0000

    edit task 9

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-unused-vars */
 export default function signUpUser(firstName, lastName) {
   return Promise.resolve({
-    firstName: '`$(firstName)`',
-    lastName: '`$(lastName)`',
+    firstName: firstName,
+    lastName: lastName,
   });
 }
 /* eslint-enable no-unused-vars */
