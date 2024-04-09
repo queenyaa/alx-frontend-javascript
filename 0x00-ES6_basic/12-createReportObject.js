36a8c0baa400dedc8a33c0c26b09de7e90a21221
queenyaa <ysgyan@gmail.com>
2024-04-09 14:05:52 +0000

    editing task 12

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -2,7 +2,7 @@ export default function createReportObject(employeesList) {
   return {
     allEmployees: employeesList,
     getNumberOfDepartments(employees) {
-        return Object.keys(employees).length;
-    }
+      return Object.keys(employees).length;
+    },
   };
 }
