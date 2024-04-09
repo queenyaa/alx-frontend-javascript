b40d8530b37ad4b235cce6528a2faa359aebde1f
queenyaa <ysgyan@gmail.com>
2024-04-09 21:08:17 +0000

    Promise chaining

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then((results) => {
       const [photoResult, userResult] = results;
       console.log(`${photoResult.body} ${userResult.firstName} ${userResult.lastName}`);
