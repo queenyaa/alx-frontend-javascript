ff91dc67d1b63534ed6409d9b66163fdc732a1e0
queenyaa <ysgyan@gmail.com>
2024-04-09 14:15:53 +0000

    fixing errors with eslint in task 13

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,6 +1,7 @@
 export default function createIteratorObject(report) {
-  const allEmployees = report.allEmployees;
-  const departments = Object.values(allEmployees).flatMap(e => e);
+  const { allEmployees } = report; // Using obj destructuring
+  const departments = Object.values(allEmployees).flatMap((e) => e);
+  // Using arrow function parenthesis
 
   let index = 0;
 
@@ -10,11 +11,13 @@ export default function createIteratorObject(report) {
         return { done: true };
       }
 
-      return { value: departments[index++], done: false };
+      const value = departments[index];
+      index += 1;
+      return { value, done: false };
     },
     [Symbol.iterator]() {
       return this;
-    }
+    },
   };
 
   return iterator;
