c7d725028de06b1d5d980c6e98b166c26c95bddb
queenyaa <ysgyan@gmail.com>
2024-04-09 23:24:37 +0000

    function to call other functions

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,21 +1,26 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const signUpPromise = signUpUser(firstName, lastName);
-  const uploadPromise = uploadPhoto(fileName);
-
-  try {
-    const signUpResult = await signUpPromise;
-    const uploadResult = await uploadPromise;
-
-    return [
-      { status: 'fulfilled', value: signUpResult },
-      { status: 'fulfilled', value: uploadResult },
-    ];
-  } catch (error) {
-    return [
-      { status: 'rejected', value: error },
-    ];
-  }
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ])
+    .then((result) => {
+      const array = [];
+      result.forEach((index) => {
+        if (index.status === 'fulfilled') {
+          array.push({
+            status: index.status,
+            value: index.value,
+          });
+        } else {
+          array.push({
+            status: index.status,
+            value: `Error: ${index.reason.message}`,
+          });
+        }
+      });
+      return array;
+    });
 }
