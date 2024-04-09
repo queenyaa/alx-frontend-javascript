20a4d76565ba61835425f26702eb686c45835556
queenyaa <ysgyan@gmail.com>
2024-04-09 22:42:44 +0000

    get user names

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-unused-vars */
 export default function signUpUser(firstName, lastName) {
   return Promise.resolve({
-    firstName: firstName,
-    lastName: lastName,
+    firstName,
+    lastName,
   });
 }
 /* eslint-enable no-unused-vars */
