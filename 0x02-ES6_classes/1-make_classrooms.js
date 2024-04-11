ce0d20c5d6fbc8c5c7b93ad6af00c81a8c4de467
queenyaa <ysgyan@gmail.com>
2024-04-11 22:10:17 +0000

    returns 3 arrays of classroom class

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,10 @@
+import ClassRoom from './0-classroom';
+
+export default function intializesRooms() {
+  const rooms = [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34),
+  ];
+  return rooms;
+}
